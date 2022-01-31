112cc222c4959eaf75806dd34492e059ad6d2b24
Doug <doug@echko.co>
2022-01-31 18:04:05 +0100

    Letsgo

--- src/pages/services/performance-marketing.js
+++ src/pages/services/performance-marketing.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { ArticleCard } from '@components/ArticleCard'
 import { Award } from '@components/Award'
 import { SignupForm } from '@components/ContactForm'
